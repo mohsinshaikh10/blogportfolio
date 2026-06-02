77b05428dcb0665afab1158543a89ebae4138691
mohsinshaikh10 <78855163+mohsinshaikh10@users.noreply.github.com>
2026-06-02 14:08:49 -0400

    Create auth.php

---
+++ includes/auth.php
@@ -0,0 +1,38 @@
+<?php
+
+if(session_status() === PHP_SESSION_NONE){
+    session_start();
+}
+
+function isLoggedIn()
+{
+    return isset($_SESSION['user_id']);
+}
+
+function isAdmin()
+{
+    return (
+        isset($_SESSION['role']) &&
+        $_SESSION['role'] === 'admin'
+    );
+}
+
+function requireLogin()
+{
+    if(!isLoggedIn())
+    {
+        header("Location: login.php");
+        exit;
+    }
+}
+
+function requireAdmin()
+{
+    if(!isAdmin())
+    {
+        header("Location: ../index.php");
+        exit;
+    }
+}
+
+?>

e0fd1105e1bff176753564442ead7b7be7a8c02f
mohsinshaikh10 <78855163+mohsinshaikh10@users.noreply.github.com>
2026-06-02 15:16:08 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include 'includes/header.php';
+?>
+
+<section class="hero">
+
+    <h1>
+        Welcome to Mohsin Blog
+    </h1>
+
+    <p>
+        Technology, Cloud Engineering,
+        AI and Personal Insights
+    </p>
+
+</section>
+
+<section class="feed">
+
+    <h2>
+        Blog Feed Coming Next...
+    </h2>
+
+</section>
+
+<?php
+include 'includes/footer.php';
+?>

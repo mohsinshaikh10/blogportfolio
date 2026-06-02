ccf76907e9f60988b721e694ead3efdfe4baf294
mohsinshaikh10 <78855163+mohsinshaikh10@users.noreply.github.com>
2026-06-02 16:12:02 -0400

    Update create-post.php

--- admin/create-post.php
+++ admin/create-post.php
@@ -1,1 +1,44 @@
+<?php
 
+include '../includes/db.php';
+include '../includes/auth.php';
+
+requireAdmin();
+
+include '../includes/header.php';
+
+?>
+
+<div class="form-container">
+
+<h2>Create Blog Post</h2>
+
+<form
+method="POST"
+action="../actions/save-post.php">
+
+<input
+type="text"
+name="title"
+placeholder="Post Title"
+required>
+
+<textarea
+name="content"
+rows="15"
+placeholder="Write your blog post..."
+required></textarea>
+
+<button type="submit">
+
+Publish Post
+
+</button>
+
+</form>
+
+</div>
+
+<?php
+include '../includes/footer.php';
+?>
